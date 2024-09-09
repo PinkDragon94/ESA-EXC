c1b7e5fce7cca3db99d717995a2a17bc64128cf3
Pinkdragon <cherandwhite@gmail.com>
2024-09-08 20:10:53 -0400

    update

--- route/index.js
+++ route/index.js
@@ -3,7 +3,16 @@ const router = express.Router();
 
 // Example route
 router.get('/', (req, res) => {
-  res.render('index'); // Render 'index.ejs' from 'views' directory
+  res.render('index'); 
+});
+router.get('/upload', (req, res) => {
+  res.render('upload'); 
+});
+router.get('/signup', (req, res) => {
+  res.render('signup'); 
+});
+router.get('/users', (req, res) => {
+  res.render('users'); y
 });
 
 module.exports = router;
